1c08429dec128def80d19bd837075f80d5e260ca
alfarabygit <alfarabyindra@gmail.com>
2023-06-10 18:22:00 +0700

    upload file pemrograman javascript

---
+++ Javascript 5/main.js
@@ -0,0 +1,44 @@
+//tipe data javascript
+
+//1. tipe data primitif
+
+//string
+let name1="John Doe";
+let name2='Alfaraby Indra';
+
+console.log(name1);
+console.log(name2);
+
+//number
+var number1=10;
+var number2=75.5;
+
+console.log(number1);
+console.log(number2);
+
+//boolean
+let isCodingFun=false;
+let isGamingFun=true;
+
+console.log(isCodingFun);
+console.log(isGamingFun);
+
+//undefined
+var title;
+
+//null
+let book=null;
+
+//2. tipe data objek
+
+//object
+let banana={
+    color: 'yellow',
+    size:"long",
+    quantity:5,
+    isSweet:true  
+};
+
+//array
+let car=["BMW","PORSCHE","FORD","AUDI","PAGANI","FERRARI","LAMBORGHINI"];
+console.log(car);
---
+++ Javascript 6/main.js
@@ -0,0 +1,20 @@
+//operator pada javascript
+let a=8;
+let b=2;
+
+let sum=a + b;
+let sub=a - b;
+let multi=a * b;
+let div=a / b;
+let mod=a % b;
+
+console.log(sum);
+console.log(sub);
+console.log(multi);
+console.log(div);
+console.log(mod);
+
+//menggabungkan string satu dengan string lainnya atau angka dengan string
+let firstname="John";
+let lastname="Doe";
+console.log(firstname+" "+lastname);
